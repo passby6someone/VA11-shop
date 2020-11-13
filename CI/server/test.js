bf94c8f31ed4f385c3bd739178a573982409f1f1
passby6someone <945879305@qq.com>
2020-11-13 23:42:11 +0800

    ci: 更改路径，完善流程

--- CI/server/test.js
+++
@@ -1,9 +0,0 @@
-
-const { deleteFolder, childProcess } = require('./util.js');
-
-async function main() {
-  await deleteFolder('CI/server/test');
-  console.log('done');
-}
-
-main();
---
+++ CI/server/util.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const child_process = require('child_process');
+
+// 递归删除文件夹
+function deleteFolder (rootPath) {
+  return new Promise((resolve, reject) => {
+    const recursionDelete = (path) => {
+      try {
+        var files = [];
+        if (fs.existsSync(path)) {
+          files = fs.readdirSync(path);
+          files.forEach(function (file, index) {
+            var curPath = path + "/" + file;
+            if (fs.statSync(curPath).isDirectory()) { // recurse
+              recursionDelete(curPath);
+            } else { // delete file
+              fs.unlinkSync(curPath);
+            }
+          });
+          fs.rmdirSync(path);
+        }
+      } catch (err) {
+        reject(err);
+      }
+      if (path === rootPath) {
+        resolve();
+      }
+    }
+    recursionDelete(rootPath);
+  });
+}
+
+function childProcess (...commend) {
+  return new Promise((resolve, reject) => {
+    childPro = child_process.spawn(...commend);
+    successChunk = [];
+    errorChunk = [];
+    childPro.stdout.on('data', (data) => successChunk.push(data));
+    childPro.stdout.on('end', () => {
+      if (successChunk.length !== 0) {
+        resolve(successChunk.join('').toString());
+      }
+    });
+
+    childPro.stderr.on('data', (data) => errorChunk.push(data));
+    childPro.stderr.on('end', () => {
+      if (errorChunk.length !== 0) {
+        reject(errorChunk.join('').toString());
+      }
+    });
+
+    childPro.on('error', (err) => reject(err));
+    childPro.on('close', (code) => resolve(code));
+  });
+}
+
+module.exports = {
+  deleteFolder,
+  childProcess,
+}
